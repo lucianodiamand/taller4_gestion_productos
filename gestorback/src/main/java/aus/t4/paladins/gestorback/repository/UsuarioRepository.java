82034be32205b7d6871c549d2a10bf6e3b42f2a3
Manuela Cepeda <manulcepeda@gmail.com>
2026-05-28 20:45:20 -0300

    ADD: modelo Usuario y RolUsuario

---
+++ gestorback/src/main/java/aus/t4/paladins/gestorback/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package aus.t4.paladins.gestorback.repository;
+
+import aus.t4.paladins.gestorback.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+}
